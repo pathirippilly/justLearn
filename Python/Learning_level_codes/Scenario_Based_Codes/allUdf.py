f7d4506c52dac49a221f29218238786361b99c78
Akhil Pathirippilly <akhilpathirippilly@gmail.com>
2021-07-12 16:44:56 +0530

    changes to allUdf.py --> Added getAllSublist

--- Python/Learning_level_codes/Scenario_Based_Codes/allUdf.py
+++ Python/Learning_level_codes/Scenario_Based_Codes/allUdf.py
@@ -1,1 +1,9 @@
-abc
+#Return list of all possible sublist from a list as input
+def getAllSublist(inpL):
+    outL=[]
+    for i in range(len(inpL)):
+        for j in range(len(inpL)+1):
+            outSubL=inpL[i:j]
+            if outSubL:
+                outL.append(outSubL)
+    return outL
---
+++ Python/Learning_level_codes/Scenario_Based_Codes/main.py
@@ -0,0 +1,8 @@
+from allUdf import getAllSublist
+#test1
+inpL=[1,2,3,4]
+print(getAllSublist(inpL))
+
+#test1 - getAllSublist
+inpL=[1,2,3,4]
+print(getAllSublist(inpL))
