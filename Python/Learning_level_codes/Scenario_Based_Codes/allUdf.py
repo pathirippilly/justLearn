a0a93dd49970a3804bd4f1a942d42d2226f3cc41
Akhil Pathirippilly <akhilpathirippilly@gmail.com>
2021-07-14 21:38:55 +0530

    Changes to allUdf.py --> added oveloaded function for getAllSublist
    Signed-off-by: Akhil Pathirippilly <akhilpathirippilly@gmail.com>

--- Python/Learning_level_codes/Scenario_Based_Codes/allUdf.py
+++ Python/Learning_level_codes/Scenario_Based_Codes/allUdf.py
@@ -1,4 +1,6 @@
 #Return list of all possible sublist from a list as input
+from multipledispatch import dispatch
+@dispatch(list)
 def getAllSublist(inpL):
     outL=[]
     for i in range(len(inpL)):
@@ -6,4 +8,20 @@ def getAllSublist(inpL):
             outSubL=inpL[i:j]
             if outSubL:
                 outL.append(outSubL)
+    return outL
+
+@dispatch(list,int)
+def getAllSublist(inpL,k):
+    outL=[]
+    for i in range(len(inpL)):
+        oddNumCounter=0
+        for j in range(len(inpL)+1):
+            outSubL=inpL[i:j]
+            if outSubL:
+                if outSubL[-1]%2!=0:
+                    oddNumCounter+=1
+                if oddNumCounter<=k:
+                    outL.append(outSubL)
+                else:
+                    break
     return outL
--- Python/Learning_level_codes/Scenario_Based_Codes/main.py
+++ Python/Learning_level_codes/Scenario_Based_Codes/main.py
@@ -1,8 +1,12 @@
 from allUdf import getAllSublist
-#test1
-inpL=[1,2,3,4]
-print(getAllSublist(inpL))
+# #test1
+# inpL=[1,2,3,4]
+# print(getAllSublist(inpL))
 
 #test1 - getAllSublist
-inpL=[1,2,3,4]
-print(getAllSublist(inpL))
+inpL=[1,2,3,4,5]
+print(getAllSublist(inpL,1))
+
+# l=[1,2,3,4]
+# l2=[1,3,4]
+k=1
